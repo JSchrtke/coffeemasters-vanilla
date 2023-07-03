264504bd772c2e61523dd1b9bce27ff2e07defba
Johannes Schrötke <j.schroetke@wertgarantie.com>
2023-07-03 14:37:43 +0200

    refactor: decouple menu from app

--- services/order.js
+++
@@ -1,8 +0,0 @@
-import API from "./api.js";
-
-export default async function loadData() {
-    const menu = await API.loadMenu();
-    console.log("loading menu");
-    console.log(menu);
-    app.store.menu = menu;
-};
