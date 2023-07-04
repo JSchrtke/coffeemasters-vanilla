cba0ffde2245631db3b4fde3c41f419272b79cec
Johannes Schrötke <j.schroetke@wertgarantie.com>
2023-07-04 13:24:00 +0200

    feat: add basic routing

---
+++ services/router.js
@@ -0,0 +1,46 @@
+const Router = {
+    init: () => {
+        document.querySelectorAll(".navlink").forEach(navlink => {
+            navlink.addEventListener("click", (clickEvent) => {
+                clickEvent.preventDefault();
+                const target = clickEvent.target.getAttribute("href")
+                Router.navigate(target);
+            });
+        });
+
+        window.addEventListener("popstate", (popStateEvent) => {
+            const target = popStateEvent.state.route;
+            Router.navigateWithoutHistory(target);
+        });
+
+        Router.navigate(location.pathname)
+    },
+
+    navigate: (route) => {
+        history.pushState({ route }, null, route);
+
+        let page = null
+        switch (route) {
+            case "/":
+                page = document.createElement("h1");
+                page.textContent = "menu";
+                break;
+            case "/order":
+                page = document.createElement("h1");
+                page.textContent = "order";
+                break;
+        }
+        if (page) {
+            const main = document.querySelector("main");
+            main.innerHTML = "";
+            main.appendChild(page);
+            window.scrollX = 0;
+        };
+    },
+
+    navigateWithoutHistory: (route) => {
+        console.log(`navigating to ${route} without history`);
+    },
+};
+
+export default Router;
